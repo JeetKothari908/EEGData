253e24f7d18d0f522e704d2474f0271a4c9a6f9f
Max Wanger <44851811+TheClockworkDungeouneer@users.noreply.github.com>
2024-03-19 13:39:52 -0500

    Fixing cross OS bug
    The python_readme  is also now out of date.
    
    command line argument `*.csv* is no longer needed.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import sys
 
-for file in [sys.argv[i] for i in range(len(sys.argv)) if i != 0]:
+import os
+
+directory_listing = [file for file in os.listdir() if file.endswith(".csv")]
+
+#sys.argv
+
+#for file in [sys.argv[i] for i in range(len(sys.argv)) if i != 0]:
+for file in directory_listing:
 
     NAME = file
     #NAME = "alexcontrol"
 
     print(f"Reading file: {NAME}")
     df = pd.read_csv(f'{NAME}')
-
+#
     new_df = df[(df["eeg_1"].isnull() | df["eeg_2"].isnull()) == False]
     #new_df = df[(df["eeg_1"].str is None or df["eeg_2"].str is None) == False]
 
